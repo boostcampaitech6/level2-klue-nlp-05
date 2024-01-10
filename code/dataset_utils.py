88413d5714461d14c2a4dfe9c95dec17e8c4c170
JaehyeokSim <116555114+simson0524@users.noreply.github.com>
2024-01-10 11:40:21 +0900

    .

--- code/dataset_utils.py
+++ code/dataset_utils.py
@@ -97,32 +97,3 @@ def num_to_label(label):
     origin_label.append(dict_num_to_label[v])
   
   return origin_label
-
-import pandas as pd
-import ast
-
-def preprocessing(df):
-    # seperate subject_entity
-    subject_word, subject_start_idx, subject_end_idx, subject_type = [], [], [], []
-    for data in df['subject_entity']:
-        data = ast.literal_eval(data)
-        subject_word.append(data['word'])
-        subject_start_idx.append(data['start_idx'])
-        subject_end_idx.append(data['end_idx'])
-        subject_type.append(data['type'])
-    df['subject_word'], df['subject_start_idx'], df['subject_end_idx'], df['subject_type'] = subject_word, subject_start_idx, subject_end_idx, subject_type
-    # seperate object_entity
-    object_word, object_start_idx, object_end_idx, object_type = [], [], [], []
-    for data in df['object_entity']:
-        data = ast.literal_eval(data)
-        object_word.append(data['word'])
-        object_start_idx.append(data['start_idx'])
-        object_end_idx.append(data['end_idx'])
-        object_type.append(data['type'])
-    df['object_word'], df['object_start_idx'], df['object_end_idx'], df['object_type'] = object_word, object_start_idx, object_end_idx, object_type
-    # drop subject_entity, object_entity
-    df.drop(columns=['subject_entity', 'object_entity'], inplace=True)
-    # add entity_pair
-    df['entity_pair'] = df['subject_type'] + "-" + df['object_type']
-
-    return df
