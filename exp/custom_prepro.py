befcaee809a34e4d4f1a4f42174b6c8f6c3e6298
shingeun <sfromshsj@naver.com>
2024-01-10 08:56:47 +0000

    1.2.0

---
+++ exp/custom_prepro.py
@@ -0,0 +1,64 @@
+from tqdm import tqdm
+import pandas as pd
+
+
+class Processor:
+    def __init__(self, args, tokenizer):
+        super().__init__()
+        self.args = args
+        self.tokenizer = tokenizer
+        self.new_tokens = ['[PER]', '[ORG]', '[DAT]', '[LOC]', '[POH]', '[NOH]']
+        self.tokenizer.add_tokens(self.new_tokens)
+        self.LABEL_TO_ID = {'no_relation': 0, 'org:top_members/employees': 1, 'org:members': 2, 'org:product': 3, 'per:title': 4, 'org:alternate_names': 5, 'per:employee_of': 6, \
+                'org:place_of_headquarters': 7, 'per:product': 8, 'org:number_of_employees/members': 9, 'per:children': 10, 'per:place_of_residence': 11, 'per:alternate_names': 12, \
+                'per:other_family': 13, 'per:colleagues': 14, 'per:origin': 15, 'per:siblings': 16, 'per:spouse': 17, 'org:founded': 18, 'org:political/religious_affiliation': 19, \
+                'org:member_of': 20, 'per:parents': 21, 'org:dissolved': 22, 'per:schools_attended': 23, 'per:date_of_death': 24, 'per:date_of_birth': 25, 'per:place_of_birth': 26, \
+                'per:place_of_death': 27, 'org:founded_by': 28, 'per:religion': 29}
+        
+
+    def tokenize(self, sentence, subj_type, obj_type, ss, se, os, oe):
+        
+        sents = []
+        subj_type , obj_type = f"[{subj_type}]", f"[{obj_type}]"
+        tokens = self.tokenizer.tokenize(sentence)
+        for i_t, token in enumerate(tokens):
+            if i_t == ss:
+                new_ss = len(sents)
+                sents.extend(['@'] + ['*'] + subj_type + ['*'] + [token])
+            if i_t == se:
+                sents.extend([token] + ['@'])
+            if i_t == os:
+                new_os = len(sents)
+                sents.extend(["#"] + ['^'] + obj_type + ['^'] + [token])
+            if i_t == oe:
+                sents.extend([token] + ['#'])
+        sents = sents[:self.args.max_seq_length - 2]
+        input_ids = self.tokenizer.convert_tokens_to_ids(sents)
+        input_ids = self.tokenizer.build_inputs_with_special_tokens(input_ids)
+        
+        return input_ids, new_ss + 1, new_os + 1
+
+    def read(self, file_in):
+        features = []
+        with open(file_in, "r") as fh:
+            data = pd.read_csv(fh)
+
+        for d in tqdm(data):
+            ss, se = d['subject_start_idx'], d['subject_end_idx']
+            os, oe = d['object_start_idx'], d['object_end_idx']
+
+            sentence = d['sentence']
+
+            input_ids, new_ss, new_os = self.tokenize(sentence, d['subject_type'], d['object_type'], ss, se, os, oe)
+            rel = self.LABEL_TO_ID[d['label']]
+
+            feature = {
+                'input_ids': input_ids,
+                'labels': rel,
+                'ss': new_ss,
+                'os': new_os,
+            }
+            features.append(feature)
+
+        return features
+    
---
+++ exp/custom_train.py
@@ -0,0 +1,109 @@
+from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification, Trainer, TrainingArguments
+from omegaconf import OmegaConf
+from dataset_utils import load_data, label_to_num, tokenized_dataset
+from datasets import RE_Dataset
+from metrics import compute_metrics
+from custom_model import Custom_Model
+from custom_prepro import Processor
+
+
+import numpy as np
+import argparse
+import random
+import torch
+import wandb
+import os
+
+
+def set_seed(seed:int = 42):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--config", "-c", type=str, default="base_config")
+
+  args, _ = parser.parse_known_args()
+  conf = OmegaConf.load(f"./config/{args.config}.yaml")
+
+  set_seed(42)
+
+  wandb.login()
+  wandb.init(project=conf.wandb.project_name)
+
+  # load model and tokenizer
+  MODEL_NAME = conf.model.model_name
+  tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+  # load dataset
+  train_dataset = load_data("../dataset/train/train.csv")
+
+  train_label = label_to_num(train_dataset['label'].values)
+
+  # tokenizing dataset
+  if MODEL_NAME.split('-')[0] == 'xlm':
+    tokenized_train = tokenized_dataset_xlm(train_dataset, tokenizer)
+  else:
+    tokenized_train = tokenized_dataset(train_dataset, tokenizer)
+
+  # make dataset for pytorch.
+  RE_train_dataset = RE_Dataset(tokenized_train, train_label)
+  
+  # Split train dataset into train, valid.
+  RE_train_dataset, RE_dev_dataset = torch.utils.data.random_split(RE_train_dataset, [int(len(RE_train_dataset)*0.8), len(RE_train_dataset)-int(len(RE_train_dataset)*0.8)])
+
+  device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+  
+  print(device)
+  # setting model hyperparameter
+  model_config =  AutoConfig.from_pretrained(MODEL_NAME)
+  model_config.num_labels = 30
+  model_config.dropout_prob = conf.model.last_dense_layer_dropout_prob
+
+  model =  , config=model_config)
+  print(model.config)
+  model.parameters
+  model.to(device)
+  
+  # 사용한 option 외에도 다양한 option들이 있습니다.
+  # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
+  training_args = TrainingArguments(
+    output_dir='./results',          # output directory
+    save_total_limit=conf.utils.top_k,  # number of total save model.
+    save_steps=conf.train.save_steps,                 # model saving step.
+    num_train_epochs=conf.train.epochs,              # total number of training epochs
+    learning_rate=conf.train.learning_rate,               # learning_rate
+    per_device_train_batch_size=conf.train.batch_size,  # batch size per device during training
+    per_device_eval_batch_size=conf.train.batch_size,   # batch size for evaluation
+    warmup_steps=conf.train.warmup_steps,                # number of warmup steps for learning rate scheduler
+    weight_decay=conf.train.weight_decay,               # strength of weight decay
+    logging_dir='./logs',            # directory for storing logs
+    logging_steps=conf.train.logging_steps,              # log saving step.
+    evaluation_strategy='steps', # evaluation strategy to adopt during training
+                                # `no`: No evaluation during training.
+                                # `steps`: Evaluate every `eval_steps`.
+                                # `epoch`: Evaluate every end of epoch.
+    eval_steps=conf.train.eval_steps,            # evaluation step.
+    load_best_model_at_end = True,
+    metric_for_best_model="micro f1 score",
+    report_to="wandb"
+  )
+  trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=RE_train_dataset,
+    eval_dataset=RE_train_dataset,
+    compute_metrics=compute_metrics
+  )
+
+  # train model
+  trainer.train()
+  model.save_pretrained('./best_model')
+  
+  wandb.finish()
