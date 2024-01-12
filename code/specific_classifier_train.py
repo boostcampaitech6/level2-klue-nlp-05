7b1b29e92218787d04b12b3dae4378b7da6c2c60
PotatoSim <simson0524@naver.com>
2024-01-12 11:32:47 +0900

    +stratifiedKFold

--- code/specific_classifier_train.py
+++ code/specific_classifier_train.py
@@ -3,6 +3,8 @@ from omegaconf import OmegaConf
 from dataset_utils import load_data, label_to_num, tokenized_dataset, tokenized_dataset_xlm
 from datasets import RE_Dataset
 from metrics import compute_metrics
+from sklearn.model_selection import StratifiedKFold
+from torch.utils.data import Subset
 
 import pandas as pd
 import numpy as np
@@ -63,11 +65,6 @@ if __name__ == '__main__':
 
     # entity pair별로 학습진행
     for pair in pair_list_train:
-        wandb.login()
-        wandb.init(project=conf.wandb.project_name,
-                   entity='level2-klue-nlp-05',
-                   name=f'{conf.wandb.curr_ver} ({pair})')
-
         # load dataset
         train_dataset = load_data(f"../dataset/train/{pair}_train.csv")
         train_label = label_to_num(train_dataset['label'].values)
@@ -84,56 +81,64 @@ if __name__ == '__main__':
         RE_train_dataset = RE_Dataset(tokenized_train, 
                                       train_label)
         
-        # Split train dataset into train, valid.
-        RE_train_dataset, RE_dev_dataset = torch.utils.data.random_split(RE_train_dataset, [int(len(RE_train_dataset)*0.8), len(RE_train_dataset)-int(len(RE_train_dataset)*0.8)])
+        # Stratofied K-Fold 설정
+        skf = StratifiedKFold(n_splits=10)
 
         device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-        
         print(device)
+
         # setting model hyperparameter
         model_config = AutoConfig.from_pretrained(MODEL_NAME)
         model_config.num_labels = 30
 
-        model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME,
-                                                                   config=model_config)
-        print(model.config)
-        model.parameters
-        model.to(device)
-        
-        # 사용한 option 외에도 다양한 option들이 있습니다.
-        # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
-        training_args = TrainingArguments(
-            output_dir='./results',          # output directory
-            save_total_limit=conf.utils.top_k,  # number of total save model.
-            save_steps=conf.train.save_steps,                 # model saving step.
-            num_train_epochs=conf.train.epochs,              # total number of training epochs
-            learning_rate=conf.train.learning_rate,               # learning_rate
-            per_device_train_batch_size=conf.train.batch_size,  # batch size per device during training
-            per_device_eval_batch_size=conf.train.batch_size,   # batch size for evaluation
-            warmup_steps=conf.train.warmup_steps,                # number of warmup steps for learning rate scheduler
-            weight_decay=conf.train.weight_decay,               # strength of weight decay
-            logging_dir='./logs',            # directory for storing logs
-            logging_steps=conf.train.logging_steps,              # log saving step.
-            evaluation_strategy='steps', # evaluation strategy to adopt during training
-                                        # `no`: No evaluation during training.
-                                        # `steps`: Evaluate every `eval_steps`.
-                                        # `epoch`: Evaluate every end of epoch.
-            eval_steps=conf.train.eval_steps,            # evaluation step.
-            load_best_model_at_end = True,
-            metric_for_best_model="micro f1 score",
-            report_to="wandb",
-            fp16=conf.train.fp16
-        )
-        trainer = Trainer(
-            model=model,
-            args=training_args,
-            train_dataset=RE_train_dataset,
-            eval_dataset=RE_dev_dataset,
-            compute_metrics=compute_metrics
-        )
+        for fold, (train_idx, dev_idx) in enumerate(skf.split(np.zeros(len(RE_train_dataset)), RE_train_dataset.labels)):
+            wandb.login()
+            wandb.init(project=conf.wandb.project_name,
+                       entity='level2-klue-nlp-05',
+                       name=f'{conf.wandb.curr_ver} ({pair} - Fold:{fold})')
+            model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME,
+                                                                       config=model_config)
+            print(model.config)
+            model.parameters
+            model.to(device)
 
-        # train model
-        trainer.train()
-        model.save_pretrained(f'./best_model/{pair}')
-        
-        wandb.finish()
+            train_subset = Subset(RE_train_dataset, train_idx)
+            dev_subset = Subset(RE_train_dataset, dev_idx)
+            
+            # 사용한 option 외에도 다양한 option들이 있습니다.
+            # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
+            training_args = TrainingArguments(
+                output_dir='./results',          # output directory
+                save_total_limit=conf.utils.top_k,  # number of total save model.
+                save_steps=conf.train.save_steps,                 # model saving step.
+                num_train_epochs=conf.train.epochs,              # total number of training epochs
+                learning_rate=conf.train.learning_rate,               # learning_rate
+                per_device_train_batch_size=conf.train.batch_size,  # batch size per device during training
+                per_device_eval_batch_size=conf.train.batch_size,   # batch size for evaluation
+                warmup_steps=conf.train.warmup_steps,                # number of warmup steps for learning rate scheduler
+                weight_decay=conf.train.weight_decay,               # strength of weight decay
+                logging_dir='./logs',            # directory for storing logs
+                logging_steps=conf.train.logging_steps,              # log saving step.
+                evaluation_strategy='steps', # evaluation strategy to adopt during training
+                                            # `no`: No evaluation during training.
+                                            # `steps`: Evaluate every `eval_steps`.
+                                            # `epoch`: Evaluate every end of epoch.
+                eval_steps=conf.train.eval_steps,            # evaluation step.
+                load_best_model_at_end = True,
+                metric_for_best_model="micro f1 score",
+                report_to="wandb",
+                fp16=conf.train.fp16
+            )
+            trainer = Trainer(
+                model=model,
+                args=training_args,
+                train_dataset=train_subset,
+                eval_dataset=dev_subset,
+                compute_metrics=compute_metrics
+            )
+
+            # train model
+            trainer.train()
+            model.save_pretrained(f'./best_model/{pair}/{fold}')
+            
+            wandb.finish()
