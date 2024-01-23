6b9bcb29e18a84fbfe77af6c2d5767a349c26110
rlarlgh96 <rlarlgh96@khu.ac.kr>
2024-01-23 17:24:29 +0000

    final

--- dataset/preprocessing.py
+++ dataset/preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import ast
 import torch
+from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 
 
@@ -41,7 +42,7 @@ def data_augmentation(df):
     tokenizer = AutoTokenizer.from_pretrained('klue/roberta-large')
     model = AutoModelForMaskedLM.from_pretrained('klue/roberta-large')
     
-    for idx, row in df.iterrows():
+    for idx, row in tqdm(df.iterrows(), total=df.shape[0]):
         sentence = row['sentence']
         subject_start_idx, object_start_idx = row['subject_start_idx'], row['object_start_idx']
         subject_end_idx, object_end_idx = row['subject_end_idx'], row['object_end_idx']
@@ -65,8 +66,7 @@ def data_augmentation(df):
             predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
             predicted_tokens.append(predicted_token)
 
-        for i, mask_index in enumerate(mask_indices):
-            new_sentence = masked_sentence.replace("[MASK]", predicted_tokens[i], 1)
+        new_sentence = masked_sentence.replace("[MASK]", predicted_tokens[0], 1).replace("[MASK]", predicted_tokens[1], 1)
         
         if ('[PAD]' in new_sentence) or ('[UNK]' in new_sentence):
             continue
